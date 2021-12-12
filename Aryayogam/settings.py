c1d84b77e8f5436f0934070e33f6123c72af3cc9
Amithesh Gupta <amithesh2099@gmail.com>
2021-12-12 10:40:38 +0530

    static fix

--- Aryayogam/settings.py
+++ Aryayogam/settings.py
@@ -138,6 +138,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media_cdn")
 
